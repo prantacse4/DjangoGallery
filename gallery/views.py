9893e03ca7d56b15069c22fd9a6fef70370ca2e5
prantacse4 <pranta.cse4.bu@gmail.com>
2021-02-08 14:08:32 +0600

    Settings Updated

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from .models import *
+from django.db.models import Q
+from  django.contrib import messages
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import ImageUploadForm
 # Create your views here.
+
+def gallery(request):
+    diction = {}
+    return render(request, 'gallery/index.html', context=diction)
